4c25a3ed968ca04faa43cbae554de8c994602b39
Salmai Safi <aman_safi@hotmail.de>
2024-09-27 10:39:06 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./main-content/header/header.component";
+import { HeroComponent } from "./main-content/hero/hero.component";
+import { AboutmeComponent } from "./main-content/aboutme/aboutme.component";
+import { SkillsComponent } from "./main-content/skills/skills.component";
+import { PortfolioComponent } from "./main-content/portfolio/portfolio.component";
+import { ReviewsComponent } from "./main-content/reviews/reviews.component";
+import { ContactComponent } from "./main-content/contact/contact.component";
+import { FooterComponent } from "./main-content/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, HeroComponent, AboutmeComponent, SkillsComponent, PortfolioComponent, ReviewsComponent, ContactComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
